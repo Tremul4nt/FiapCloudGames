703694188138083f97c6d3c727bed60047813467
Roberto <alves.robalves@gmail.com>
2025-06-02 19:37:32 -0300

    Ajuste Imports

--- FCG.Core/Entity/Biblioteca.cs
+++ FCG.Core/Entity/Biblioteca.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FCG.Core.Entity
+﻿namespace FCG.Core.Entity
 {
     public class Biblioteca: EntityBase
     {
